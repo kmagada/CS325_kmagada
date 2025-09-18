3697a05fc02e3d1ad34dc4c5298c03cb38bdc21d
kmagada <kmagada@siue.edu>
2025-09-18 10:23:24 -0500

    main api works, requirements.txt update, as well as more dockerfile shenanigans

--- project1/proj/main.py
+++ project1/proj/main.py
@@ -1,15 +1,38 @@
+import json
+import ssl
 import http.client
 
-conn = http.client.HTTPSConnection("li-data-scraper.p.rapidapi.com")
 
-headers = {
-    'x-rapidapi-key': "a7d9c93d88mshd416eddf6bc76e8p1f29efjsn9578710bfc4c",
-    'x-rapidapi-host': "li-data-scraper.p.rapidapi.com"
-}
+def create_ssl_context():
+    try:
+        import certifi
+        cafile = certifi.where()
+        return ssl.create_default_context(cafile=cafile)
+    except Exception:
+        return ssl.create_default_context()
 
-conn.request("GET", "/get-profile-data-by-url?url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fadamselipsky%2F", headers=headers)
 
-res = conn.getresponse()
-data = res.read()
+def main():
+    ctx = create_ssl_context()
+    try:
+        conn = http.client.HTTPSConnection("jsearch.p.rapidapi.com", context=ctx)
+        headers = {
+            'x-rapidapi-key': "a7d9c93d88mshd416eddf6bc76e8p1f29efjsn9578710bfc4c",
+            'x-rapidapi-host': "jsearch.p.rapidapi.com"
+        }
+        conn.request("GET", "/search?query=developer%20jobs%20in%20chicago&page=1&num_pages=1&country=us&date_posted=all", headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        with open("data.json", "w", encoding="utf-8") as f:
+            json.dump(json.loads(data.decode("utf-8")), f, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        try:
+            conn.close()
+        except Exception:
+            pass
 
-print(data.decode("utf-8"))
+
+if __name__ == '__main__':
+    main()
