493c89f0c6833a51be72d0a4d268e03d72ff42d1
kmagada <kmagada@siue.edu>
2025-10-06 13:52:39 -0500

    datapull works now

--- project1/proj/main.py
+++ project1/proj/main.py
@@ -1,13 +1,7 @@
 import datapull
 
 def main():
-    print("Calling datapull.main() from datapull.py...")
     datapull.main()
-    print("We Ran the Data Pull Script")
-
-
-
-
 
 # add embedding here
 # text-embedding-3-small or text-embedding-3-large
@@ -19,3 +13,6 @@ def main():
 # job listing embedding
 
 # add Top N Selection here for top 10 jobs for the resume
+
+if __name__ == '__main__':
+    main()
