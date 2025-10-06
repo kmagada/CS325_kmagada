fb07f629d23f65acaa2d74946be30c57de58c093
kmagada <kmagada@siue.edu>
2025-10-06 14:35:53 -0500

    started on embedding

---
+++ project1/proj/embedding.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+
+
+client = OpenAI()
+
+
+# text-embedding-3-small or text-embedding-3-large
+response = client.embeddings.create(
+    input="Your text string goes here",
+    model="text-embedding-3-small"
+)
+print(response.data[0].embedding)
+
+
+
+
+'''
+# FROM OPENAI RECOMMANDATION STRING THING
+def recommendations_from_strings(
+    strings: List[str],
+    index_of_source_string: int,
+    model="text-embedding-3-small",
+) -> List[int]:
+    """Return nearest neighbors of a given string."""
+
+    # get embeddings for all strings
+    embeddings = [embedding_from_string(string, model=model) for string in strings]
+
+    # get the embedding of the source string
+    query_embedding = embeddings[index_of_source_string]
+
+    # get distances between the source embedding and other embeddings (function from embeddings_utils.py)
+    distances = distances_from_embeddings(query_embedding, embeddings, distance_metric="cosine")
+
+    # get indices of nearest neighbors (function from embeddings_utils.py)
+    indices_of_nearest_neighbors = indices_of_nearest_neighbors_from_distances(distances)
+    return indices_of_nearest_neighbors
+'''
--- project1/proj/main.py
+++ project1/proj/main.py
@@ -1,18 +1,15 @@
 import datapull
+import embedding
+
 
 def main():
+    #pulls data with the API and saves to a JSON file
     datapull.main()
 
-# add embedding here
-# text-embedding-3-small or text-embedding-3-large
-
-
-# resume embedding
-
-
-# job listing embedding
-
-# add Top N Selection here for top 10 jobs for the resume
+    #embeds the resume and job listings
+    embedding.main()
+    
+# add Top N Selection here for top 10 jobs for the resume maybe use the OpenAI embedding to show recommendations
 
 if __name__ == '__main__':
     main()
